d832beef0af1c3590960a7415ca421749f19cc1a
Katy DeCorah <kdecorah@gmail.com>
2013-06-01 19:55:38 -0400

    added title of image, few tweaks to api

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,26 @@
-var apiMedium = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633859537779&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_z";
+var apiSmall = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633859537779&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_n";
 
-$.getJSON(apiMedium,function(json){
-	$.each(json.photoset.photo,function(i,myresult){
-		$("body").css("background-image", "url(" + myresult.url_z + ")");
+$.getJSON(apiSmall,function(json){
+	$.each(json.photoset.photo,function(i,item){
+		var inSet = json.photoset.photo.length - 1;
+		if (i === inSet) {
+			$("body").css("background-image", "url(" + item.url_n + ")");
+			$(".title").text(item.title);
+		}
 	});
 });
 
-function sniff() {
+//function sniff() {
 
 	if ( $(window).width() < 500) {
 		var apiSmall = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633859537779&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_n";
+
 		$.getJSON(apiSmall,function(json){
-			$.each(json.photoset.photo,function(i,myresult){
-				$("body").css("background-image", "url(" + myresult.url_n + ")");
+			$.each(json.photoset.photo,function(i,item){
+				var inSet = json.photoset.photo.length - 1;
+				if (i === inSet) {
+					$("body").css("background-image", "url(" + item.url_n + ")");
+				}
 			});
 		});
 	}
@@ -22,30 +30,41 @@ function sniff() {
 		var apiMedium = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633859537779&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_z";
 
 		$.getJSON(apiMedium,function(json){
-			$.each(json.photoset.photo,function(i,myresult){
-				$("body").css("background-image", "url(" + myresult.url_z + ")");
+			$.each(json.photoset.photo,function(i,item){
+				var inSet = json.photoset.photo.length - 1;
+				if (i === inSet) {
+					$("body").css("background-image", "url(" + item.url_z + ")");
+				}
 			});
 		});
 	}
 
 	else {
-		var apiLarge = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633859537779&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_o";
 
+
+		var apiLarge = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633859537779&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_o";
 
 		$.getJSON(apiLarge,function(json){
 			$("body").append("<div class='helper'></div>");
-			$.each(json.photoset.photo,function(i,myresult){
-				$(".helper").css("background-image", "url(" + myresult.url_o + ")");
-				$(window).load(function() {
-					$("body").css("background-image", "url(" + myresult.url_o + ")");
-					$(".helper").remove();
-				});
+			$.each(json.photoset.photo,function(i,item){
+				var inSet = json.photoset.photo.length - 1;
+				if (i === inSet) {
+					$(".helper").css("background-image", "url(" + item.url_o + ")");
+					$(window).load(function() {
+						$("body").css("background-image", "url(" + item.url_o + ")");
+						$(".helper").remove();
+					});
+				}
 			});
 		});
-	}
-}
-sniff();
 
-$(window).resize(function() {
-	sniff();
-});
+
+
+	}
+//}
+//sniff();
+
+//$(window).resize(function() {
+//	sniff();
+//});
+
