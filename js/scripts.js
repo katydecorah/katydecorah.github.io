e2fff15f60aaae14a441f5c2c3298926a0dde19a
Katy DeCorah <kdecorah@gmail.com>
2013-05-31 22:37:08 -0400

    sniff width for the api call

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,37 @@
-var apiLarge = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633712096580&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_o";
+function sniff() {
 
-var apiSmall = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633712096580&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_n";
+	if ( $(window).width() < 500) {
+		var apiSmall = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633712096580&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_n";
+		$.getJSON(apiSmall,function(json){
+			$.each(json.photoset.photo,function(i,myresult){
+				$("body").css("background-image", "url(" + myresult.url_n + ")");
+			});
+		});
+	}
 
-$(document).ready(function(){
-  
-  if ( $(window).width() < 800) {
-    $.getJSON(apiSmall,function(json){
-      $.each(json.photoset.photo,function(i,myresult){
-        $("body").css("background-image", "url(" + myresult.url_n + ")");
-      });
-    });
-  }
-  else {
-    $.getJSON(apiLarge,function(json){
-      $.each(json.photoset.photo,function(i,myresult){
-        $("body").css("background-image", "url(" + myresult.url_o + ")");
-      });
-    });
-  }
-  
+	else if ( $(window).width() < 800) {
+		var apiMedium = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633712096580&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_z";
+
+		$.getJSON(apiMedium,function(json){
+			$.each(json.photoset.photo,function(i,myresult){
+				$("body").css("background-image", "url(" + myresult.url_z + ")");
+			});
+		});
+	}
+
+	else {
+		var apiLarge = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633712096580&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_o";
+
+
+		$.getJSON(apiLarge,function(json){
+			$.each(json.photoset.photo,function(i,myresult){
+				$("body").css("background-image", "url(" + myresult.url_o + ")");
+			});
+		});
+	}
+}
+sniff();
+
+$(window).resize(function() {
+	sniff();
 });
