afaa105245e52766f06fafae7e3213e7753f4501
Katy DeCorah <kdecorah@gmail.com>
2013-05-31 22:11:44 -0400

    background image optim for smaller sizes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
-var apiurl = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633712096580&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_o";
+var apiLarge = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633712096580&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_o";
+
+var apiSmall = "http://api.flickr.com/services/rest/?format=json&method=flickr.photosets.getPhotos&photoset_id=72157633712096580&api_key=1e7b492b2667c5dcff54a1ba2e071ef3&format=json&jsoncallback=?&extras=url_n";
+
 $(document).ready(function(){
-    $.getJSON(apiurl,function(json){
+  
+  if ( $(window).width() < 800) {
+    $.getJSON(apiSmall,function(json){
+      $.each(json.photoset.photo,function(i,myresult){
+        $("body").css("background-image", "url(" + myresult.url_n + ")");
+      });
+    });
+  }
+  else {
+    $.getJSON(apiLarge,function(json){
       $.each(json.photoset.photo,function(i,myresult){
         $("body").css("background-image", "url(" + myresult.url_o + ")");
       });
     });
+  }
+  
 });
